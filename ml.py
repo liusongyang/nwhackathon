4f43ab1318259ae2cc5a3668880561dea44e9774
liusongyang <liusong.yang@nationwide.com>
2016-07-18 22:01:54 -0400

    added a rest api initially

---
+++ ml.py
@@ -0,0 +1,33 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+from flask import make_response
+from flask import request
+ml = Flask(__name__)
+
+points = [
+    {
+        'time': 1,
+        'temp': 60,
+        'done': False
+    },
+    {
+        'id': 2,
+        'temp': 50,
+        'done': False
+    }
+]
+
+@ml.route('/todo/api/ml', methods=['POST'])
+def doML():
+    if not request.json or not 'temp' in request.json:
+        abort(400)
+    temp = {
+        'id': points[-1]['id'] + 1,
+        'temp': request.json['temp'],
+        'done': True
+    }
+    points.append(temp)
+    return jsonify({'temp': temp}), 201
+	
+if __name__ == '__main__':
+    ml.run(debug=True)
